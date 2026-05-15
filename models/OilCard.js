d99c30f4d27479d30931a7e057e47188c610a11f
Bastiesp <bastian.ef89@gmail.com>
2026-05-14 22:20:14 -0400

    Add files via upload

--- models/OilCard.js
+++ models/OilCard.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const oilCardSchema = new mongoose.Schema({
-  ownerName: String, brand: String, model: String, year: Number,
-  currentKm: Number, nextKm: Number, oilUsed: String, notes: String,
+  vehicle: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' },
+  ownerName: String,
+  brand: String,
+  model: String,
+  year: Number,
+  currentKm: Number,
+  nextKm: Number,
+  oilUsed: String,
+  notes: String,
   date: { type: Date, default: Date.now }
 }, { timestamps: true });
 module.exports = mongoose.model('OilCard', oilCardSchema);
